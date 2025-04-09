9aa52aac1d21d4dbd0ffa118b5a44520e1d90919
Channa2146@gmail.com <Channa2146@gmail.com>
2025-04-10 00:47:36 +0530

    nginx.conf and setup.sh added

--- system-core/frontend/src/plugin/core/executors/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BackgroundPluginExecutor } from './backgroundScriptExecutor';
-export { default as SummaryViewExecutor } from './summaryViewExecutor';
-export { default as ExpandViewExecutor } from './expandViewExecutor';
--- system-core/frontend/src/plugin/core/loaders/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BasePluginLoader } from "./basePluginLoader";
-export { default as ExternalPluginLoader } from "./externalPluginLoader";
-export { default as InternalPluginLoader } from "./internalPluginLoader";
